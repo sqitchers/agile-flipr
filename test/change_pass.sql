93ab5db93c47ca2e5a8ff416d6c3508f51d478a9
David E. Wheeler <david@justatheory.com>
2016-01-26 17:36:16 -0800

    Add `change_pass()`.

---
+++ test/change_pass.sql
@@ -0,0 +1,52 @@
+SET client_min_messages TO warning;
+CREATE EXTENSION IF NOT EXISTS pgtap;
+RESET client_min_messages;
+SET search_path TO flipr,public;
+
+BEGIN;
+--SELECT no_plan();
+SELECT plan(9);
+
+SELECT has_function( 'change_pass' );
+SELECT has_function(
+    'change_pass', ARRAY['text', 'text', 'text']
+);
+SELECT function_lang_is(
+    'change_pass', ARRAY['text', 'text', 'text'],
+    'plpgsql'
+);
+SELECT function_returns(
+    'change_pass', ARRAY['text', 'text', 'text'],
+    'boolean'
+);
+SELECT volatility_is(
+    'change_pass', ARRAY['text', 'text', 'text'],
+    'volatile'
+);
+
+SELECT insert_user('theory', 'foo');
+
+SELECT ok(
+    change_pass('theory', 'foo', 'bar'),
+    'Changing a password should return true'
+);
+
+SELECT row_eq(
+   'SELECT * FROM users',
+   ROW('theory', md5('bar'), NOW())::users,
+   'The password should have been changed'
+);
+
+SELECT ok(
+    NOT change_pass('theory', 'foo', 'bar'),
+    'Bad old password should return false'
+);
+
+SELECT row_eq(
+   'SELECT * FROM users',
+   ROW('theory', md5('bar'), NOW())::users,
+   'The password should be unchanged'
+);
+
+SELECT finish();
+ROLLBACK;
